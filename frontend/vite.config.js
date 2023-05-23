bdbce755bc2c59302a55c3b2164bfe379e7f2cc0
stemG <77492060+gkana19@users.noreply.github.com>
2023-05-24 02:56:36 +0700

    remove proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,17 +4,17 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://secure-backend-production.up.railway.app/",
-        // target: "https://backend-btlz.onrender.com",
-        // target: "http://127.0.0.1:5000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://secure-backend-production.up.railway.app/",
+  //       // target: "https://backend-btlz.onrender.com",
+  //       // target: "http://127.0.0.1:5000",
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
   optimizeDeps: {
     exclude: ["react-notification-badge"],
   },
