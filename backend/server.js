d9dcad4c891bec4a380a60b923d2c556097d8f6e
stemG <77492060+gkana19@users.noreply.github.com>
2023-05-24 04:22:15 +0700

    change port

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.use("/api/message", messageRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 const server = http.createServer(app);
 
