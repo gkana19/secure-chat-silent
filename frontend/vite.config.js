0209bafbedd5ff65a159002340ab7b6b0d962951
stemG <77492060+gkana19@users.noreply.github.com>
2023-05-24 05:31:59 +0700

    edit proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,31 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { createServer } from "vite";
+import { EventEmitter } from "events";
 
-// const server = createServer({
-//   middleware: [
-//     (req, res, next) => {
-//       res.setHeader("Access-Control-Allow-Origin", "*");
-//       res.setHeader(
-//         "Access-Control-Allow-Methods",
-//         "GET, POST, PUT, DELETE, OPTIONS"
-//       );
-//       res.setHeader(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//       );
-//       next();
-//     },
-//   ],
-// });
+EventEmitter.defaultMaxListeners = 20;
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    // middlewareMode: true,
-    // server,
     proxy: {
-      "/api": {
+      "https://mellifluous-meerkat-c576c7.netlify.app/api": {
         target: "https://secure-backend-production.up.railway.app",
         changeOrigin: true,
       },
