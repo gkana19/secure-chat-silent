98bd365bc1db4804c50b128e160e084a15a56933
stemG <77492060+gkana19@users.noreply.github.com>
2023-05-24 00:09:13 +0700

    edit server cors

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5173", // for react dev http://localhost:3000
+    origin: "https://backend-btlz.onrender.com", //"http://localhost:5173", // for react dev http://localhost:3000
   },
 });
 
